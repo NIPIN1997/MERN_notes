4bf1e886e24acc0c13e99e550399b77914329cfd
Nipin <nipingeorge1997@gmail.com>
2024-01-20 14:23:15 +0530

    javascript 20-01-2024

---
+++ Mern HTML/javascript_20-01-2024/index.js
@@ -0,0 +1,66 @@
+//String data types
+console.log("String data types");
+var a1="hello";
+let a2="world";
+const a3="morning";
+console.log(a1);
+console.log(a2);
+console.log(a3);
+//Numeric data types
+console.log("Numeric data types");
+var b1=10;
+let b2=20;
+const b3=5;
+console.log(b1);
+console.log(b2);
+console.log(b3);
+//Boolean data types
+console.log("Boolean data types");
+var c1=true;
+let c2=false;
+console.log(c1);
+console.log(c2);
+//null
+console.log("null data type");
+var d1=null;
+let d2=null;
+const d3=null;
+console.log(d1);
+console.log(d2);
+console.log(d3);
+//undefined
+console.log("undefined data type");
+var e1;
+let e2
+//const e3; const variable must be initialized
+console.log(e1);
+console.log(e2);
+//NaN
+console.log("NaN");
+let g1="hello";
+let g2=2;
+console.log(g1/g2);
+//Arithmetic operations
+console.log("Arithmetic operations");
+let i=5;
+let j=2;
+console.log(i+"+"+j+"="+(i+j));
+console.log(i+"-"+j+"="+(i-j));
+console.log(i+"*"+j+"="+(i*j));
+console.log(i+"/"+j+"="+(i/j));
+console.log(i+"%"+j+"="+(i%j));
+console.log(i+"**"+j+"="+(i**j));
+//Comparison operators
+console.log("Comparison Operators");
+console.log("5>2 : "+(5>2));
+console.log("5<2 : "+(5<2));
+console.log("5>=2 : "+(5>=2));
+console.log("2>=2 : "+(2>=2));
+console.log("1<=2 : "+(1<=2));
+console.log("2<=2 : "+(2<=2));
+console.log("2==2 : "+(2==2));
+console.log("\'2\'===2 : "+('2'===2));
+console.log("3!=2 : "+(3!=2));
+console.log("\'3\'!==2 : "+('3'!==2));
+console.log("2!=2 : "+(2!=2));
+console.log("\'2\'!==2 : "+('2'!==2));
